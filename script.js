e5588e0aa9de38f213eb411367b3151df5387d40
Azrielle <azriellepelayo@gmail.com>
2020-06-09 17:51:53 +0800

    Removed operator class Made numbers and operator appear on the screen display

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
+let equation = document.querySelector(".equation");
+let inputAndResult = document.querySelector(".inputAndResult");
+let special = document.querySelector(".special");
+let operators = document.querySelector(".operation");
+let numbers = document.querySelector(".numbers");
+let result = 0;
+
+let eqn = "";
+numbers.addEventListener("click", getNum => {
+        
+    if(getNum.target !== getNum.currentTarget){
+        eqn += getNum.target.textContent;
+        inputAndResult.textContent = eqn;
+    }
+   
+    getNum.stopPropagation();
+});
+
+operators.addEventListener("click", getOp=>{
+    if(getOp.target !== getOp.currentTarget){
+        if (getOp.target.className !== "equals"){
+      
+            eqn += getOp.target.textContent;
+            inputAndResult.textContent = eqn;
+        }
+        else{
+            equation.textContent = eqn;
+            let split = eqn.split(`${operation}`);
+            let firstNum = Number(split[0]);
+            let secondNum = Number(split[1]);
+            operate(firstNum, secondNum, getOp.target.className)
+        }
+        
+    }
+    
+    getOp.stopPropagation();
+})
 
 function operate(firstNum, secondNum, operator){
-    let result = 0;
+
     if (operator == "add"){
         result = add(firstNum, secondNum);
     }
@@ -13,7 +50,7 @@ function operate(firstNum, secondNum, operator){
     else if (operator == "divide"){
         result = divide(firstNum, secondNum);
     }
-    return result;
+    inputAndResult.textContent = result;
 }
 
 function add(addend1, addend2){
@@ -30,4 +67,10 @@ function multiply(multiplicand, multiplier){
 
 function divide(dividend, divisor){
     return dividend / divisor;
+}
+
+function clear(){
+    result = 0;
+    firstNum = 0;
+    secondNum = 0;
 }
